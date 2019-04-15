88481223afc7f5e3a16a658828579ee58683eb25
lydiaouzar <ouzarlydia@gmail.com>
2019-04-15 02:20:37 +0200

    Initial commit of all files to the repository

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {NavController} from '@ionic/angular';
+import {Tab1Service} from '../../app/services/tab1.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+  
+})
+export class Tab1Page {
+ 
+}
+
+  
+  
+
---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+  constructor(public navCtrl: NavController){}
+}
